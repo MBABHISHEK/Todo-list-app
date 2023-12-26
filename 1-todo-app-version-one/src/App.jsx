c297566d746bd20f28261a8395323dcc5b6bc601
ABHI <mba384420@gmail.com>
2023-12-26 13:11:35 +0530

    updated

--- 1-todo-app-version-one/src/App.jsx
+++ 1-todo-app-version-one/src/App.jsx
@@ -1,6 +1,7 @@
 import AppName from "./components/AppName"
 import TodoAdd from "./components/TodoAdd"
 import TodoItem1 from "./components/TodoItem1"
+import TodoItem2 from "./components/TodoItem2"
 function App() {
 
   return (
@@ -8,7 +9,7 @@ function App() {
       <AppName></AppName>
       <TodoAdd></TodoAdd>
       <TodoItem1></TodoItem1>
-      <TodoItem1></TodoItem1>
+      <TodoItem2></TodoItem2>
     </center>
   )
 }
---
+++ 1-todo-app-version-one/src/components/TodoItem2.jsx
@@ -0,0 +1,19 @@
+function TodoItem2(){
+
+    let todoName ='Goto to college';
+    let todoDate="4/10/2023";
+
+    return(
+        <div class='container text-center'>
+       <div class='row'>
+        <div class='col-6'>{todoName}</div>
+        <div class='col-4'>{todoDate}</div>
+         <div class='col-2'>
+          <button type='button' class='btn btn-danger'>DELETE</button>
+          </div>
+       </div>
+      </div>
+    )
+}
+
+export default TodoItem2
