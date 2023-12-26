e8f6f8264337e5e89611d6fecc0053162d0ee597
ABHI <mba384420@gmail.com>
2023-12-26 13:10:05 +0530

    new components

--- 1-todo-app-version-one/src/App.jsx
+++ 1-todo-app-version-one/src/App.jsx
@@ -1,31 +1,14 @@
 import AppName from "./components/AppName"
 import TodoAdd from "./components/TodoAdd"
+import TodoItem1 from "./components/TodoItem1"
 function App() {
 
   return (
     <center class='todo-container'>
       <AppName></AppName>
       <TodoAdd></TodoAdd>
-      <div class='container text-center'>
-       <div class='row'>
-        <div class='col-6'>GYM</div>
-         <div class='col-4'>4/10/2023
-          </div>
-         <div class='col-2'>
-          <button type='button' class='btn btn-danger'>DELETE</button>
-          </div>
-       </div>
-      </div>
-      <div class='container text-center'>
-       <div class='row'>
-        <div class='col-6'>GYM</div>
-         <div class='col-4'>4/10/2023
-          </div>
-         <div class='col-2'>
-          <button type='button' class='btn btn-danger'>DELETE</button>
-          </div>
-       </div>
-      </div>
+      <TodoItem1></TodoItem1>
+      <TodoItem1></TodoItem1>
     </center>
   )
 }
